d0ddc15a142ea70e9dd1f41139a313886ce61cc3
aletheus ang <aletheus.ang@gmail.com>
2025-08-16 14:46:48 +0800

    Initial Commit

---
+++ MemeMatch/MemeMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeMatchApp.swift
+//  MemeMatch
+//
+//  Created by Aletheus Ang on 16/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MemeMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
